411d212ba9c2bca2b3ec0476a066f2b68cf51ed1
ToHacking <amtohacking@gmail.com>
2024-04-14 17:01:30 -0700

    feat: game sdk, zk circuits

---
+++ circuits/terminal_interface/src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+use zk::game::{get_fib_number_0, get_fib_number_1, init, step, GameState};
+
+fn print_number(stdout: &mut std::io::Stdout) {
+    let nubmer0 = get_fib_number_0();
+    let nubmer1 = get_fib_number_1();
+    writeln!(stdout, "Fib number: {} {}\r", nubmer0, nubmer1).unwrap();
+}
+
+fn main() {
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    init();
+
+    print_number(&mut stdout);
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Left => step(0),
+            Key::Right => step(1),
+            Key::Char('q') => break, // Quit the game
+            _ => continue,
+        }
+        print_number(&mut stdout);
+    }
+}
