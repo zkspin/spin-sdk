a446270c6551284c28d038cd93d8f379977f4e03
tohacking <152132683+tohacking@users.noreply.github.com>
2024-06-05 16:19:59 -0700

    feat: fixed dry-run

--- sdk/gameplay/provable_game_logic/dry-run.sh
+++ sdk/gameplay/provable_game_logic/dry-run.sh
@@ -1,16 +1,14 @@
-
+RUST_BACKTRACE=full ../../../../g2/zkwasm/target/debug/zkwasm-cli --params "./params" wasm_output setup --wasm pkg/gameplay_bg.wasm
 ## TODO: move this using npx spinsdk
 RUST_BACKTRACE=full ../../../../g2/zkwasm/target/debug/zkwasm-cli --params "./params" wasm_output dry-run --wasm pkg/gameplay_bg.wasm --output ./output \
---public \
-1:i64 \
-1:i64 \
---private \
-8:i64 \
-1:i64 \
-1:i64 \
-1:i64 \
-1:i64 \
-1:i64 \
-1:i64 \
-1:i64 \
-1:i64
+--public 1:i64 \
+--public 1:i64 \
+--private 8:i64 \
+--private 1:i64 \
+--private 1:i64 \
+--private 1:i64 \
+--private 1:i64 \
+--private 1:i64 \
+--private 1:i64 \
+--private 1:i64 \
+--private 1:i64
