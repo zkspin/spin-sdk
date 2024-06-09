f5f777611628b8e2d026409901ea8c2d1b1a40fc
tohacking <152132683+tohacking@users.noreply.github.com>
2024-06-09 16:26:33 -0700

    doc

--- sdk/gameplay/provable_game_logic/src/gameplay.rs
+++ sdk/gameplay/provable_game_logic/src/gameplay.rs
@@ -6,7 +6,7 @@ use wasm_bindgen::prelude::*;
 pub const MAX_POSITION: u64 = 10;
 pub static GAME_STATE: Lazy<Mutex<RustGameState>> = Lazy::new(|| Mutex::new(RustGameState::new()));
 
-/* STATEFUL FUNCTIONS */
+/* STATEFUL FUNCTIONS This defines the initialization of the game*/
 #[wasm_bindgen]
 pub fn initialize_game(total_steps: u64, current_position: u64) {
     let mut game_state = GAME_STATE.lock().unwrap();
@@ -15,6 +15,7 @@ pub fn initialize_game(total_steps: u64, current_position: u64) {
     game_state.current_position = current_position;
 }
 
+/* STATEFUL FUNCTIONS This is defines the logic when player moves one step/entering one command*/
 #[wasm_bindgen]
 pub fn step(input: u64) {
     let mut game_state = GAME_STATE.lock().unwrap();
@@ -40,13 +41,14 @@ pub fn step(input: u64) {
     };
 }
 
-/* PURE FUNCTIONS */
+/* PURE FUNCTIONS This function returns the game state, but parse into Json, so can be used in Javascript */
 #[wasm_bindgen]
 pub fn get_game_state() -> String {
     let game = _get_game_state();
     serde_json::to_string(&game).unwrap()
 }
 
+/* PURE FUNCTION This function returns the game state, to be used in Rust and Zkmain */
 pub fn _get_game_state() -> RustGameState {
     let game = GAME_STATE.lock().unwrap().clone();
     return game;
--- sdk/gameplay/provable_game_logic/src/zkmain.rs
+++ sdk/gameplay/provable_game_logic/src/zkmain.rs
@@ -7,30 +7,35 @@ use crate::definition::RustGameState;
 
 use crate::gameplay::{_get_game_state, initialize_game, step};
 
+pub fn spin() {
+    // specify the public inputs
+    let public_input_0_total_steps: u64 = unsafe { wasm_input(1) };
+    let public_input_1_current_position: u64 = unsafe { wasm_input(1) };
+
+    initialize_game(public_input_0_total_steps, public_input_1_current_position);
+
+    // specify the private inputs
+    let private_inputs_length = unsafe { wasm_input(0) };
+
+    for _i in 0..private_inputs_length {
+        let _private_input = unsafe { wasm_input(0) };
+        step(_private_input);
+    }
+}
+
 /*
     PUBLIC INPUTS marked by `wasm_input`: e.g wasm_input(1) specifies a public input of type u64
-    PRIVATE INPUTS marked by `wasm_input`: e.g wasm_input(0) specifies a private input of type u64
+    PRIVATE INPUTS marked by `wasm_input`: e.g wasm_input(0) specifies a priva  te input of type u64
     PUBLIC OUTPUTS marked by `wasm_output`: e.g wasm_output(var) specifies an output `var` of type u64
 */
 #[wasm_bindgen]
 pub fn zkmain() -> i64 {
-    // specify the public inputs
-    let total_steps: u64 = unsafe { wasm_input(1) };
-    let current_position: u64 = unsafe { wasm_input(1) };
-
-    initialize_game(total_steps, current_position);
-
-    // specify the private inputs
-    let input_length = unsafe { wasm_input(0) };
-
-    for _i in 0..input_length {
-        let input_length = unsafe { wasm_input(0) };
-        step(input_length);
-    }
+    spin();
 
     unsafe {
         let final_game_state: RustGameState = _get_game_state();
         zkwasm_rust_sdk::dbg!("final_game_state: {}\n", final_game_state);
+
         // specify the output
         wasm_output(final_game_state.total_steps as u64);
         wasm_output(final_game_state.current_position as u64);
